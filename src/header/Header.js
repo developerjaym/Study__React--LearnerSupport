3ef473d30d58fa21d46b7510cb9360f190e5bc74
Jay M <jaymansmann@yahoo.com>
2023-01-25 22:03:17 -0500

    Can navigate to home page more easily

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,20 +1,18 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import CreatePost from "../article/post/create/form/CreateQuestionForm";
 import LoginModal from "../authenticate/modal/LoginModal";
 import SignUpModal from "../authenticate/modal/SignUpModal";
-import Dialog from "../dialog/Dialog";
 import logo from "../logo.jpg";
 import "./Header.css";
 
 export default function Header() {
-  const navigator = useNavigate()
+  const navigate = useNavigate()
   const [isLoginOpen, setLoginOpen] = useState(false);
   const [isSignUpOpen, setSignUpOpen] = useState(false);
   return (
     <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" height="24" width="24" />
-      <h1 className="app__title">Learner Support</h1>
+      <img onClick={e => navigate("/")} src={logo} className="App-logo" alt="logo" height="24" width="24" />
+      <h1 onClick={e => navigate("/")} className="app__title">Learner Support</h1>
       <label className="search-bar">
         Search
         <input type="search" />
@@ -25,7 +23,7 @@ export default function Header() {
       <button className="button" onClick={(e) => setSignUpOpen(true)}>
         Sign Up
       </button>
-      <button className="button" onClick={(e) => navigator("/ask")}>
+      <button className="button" onClick={(e) => navigate("/ask")}>
         +Ask Question
       </button>
       <SignUpModal
